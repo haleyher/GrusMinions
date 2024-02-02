3ca624177d033801e538e263449ab38ae2651d54
Haley <haleyh103@hotmail.com>
2024-02-02 15:14:36 -0500

    PLS COMMIT IM BEGGING

--- recyclequest/app/(tabs)/index.tsx
+++ recyclequest/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
@@ -6,9 +6,10 @@ import { Text, View } from '@/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Recycle Quest</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
+      {/* <Button onPress={()=> navigation.navigate("two")} title="Enter" color="#841584" /> */}
     </View>
   );
 }
