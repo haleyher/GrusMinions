ffbebf22021adb1354ace0da14595a125da9176c
Haley <haleyh103@hotmail.com>
2024-02-06 17:00:35 -0500

    pls commit in the right spot

--- recyclequest/app/(tabs)/index.tsx
+++ recyclequest/app/(tabs)/index.tsx
@@ -1,15 +1,28 @@
-import { Button, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Pressable, Button} from 'react-native';
+import { Text, View} from '@/components/Themed';
+import { Link } from 'expo-router';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
+export default function TabOneScreen(props: { onPress: any; title?: "Sign In" | undefined; }) {
+  const { onPress, title = 'Sign In' } = props;
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Recycle Quest</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
       {/* <Button onPress={()=> navigation.navigate("two")} title="Enter" color="#841584" /> */}
+      {/* <a onClick={() => { document.location.href = "three.tsx"; }}>Go to Profile</a>   */}
+      {/* <Link href="/two" asChild> 
+      <Pressable> <Text>About</Text> </Pressable></Link> */}
+      
+      <Link href="/two" asChild>
+      <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+    </Link>
+    <Button 
+    title="Press me"
+        color="#f194ff" onPress={onPress}/>
+     
     </View>
   );
 }
@@ -28,5 +41,27 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    elevation: 3,
+    backgroundColor: '#1B1F3B',
+    shadowColor: 'darkorange',
+    shadowOffset: {height: -10, width: 100},
+    marginBottom: 25,
+    borderRadius: 12,
+    border: "none",
+    color: "black",
+
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
