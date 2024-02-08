53024e5609495212f349cd742b42a46d7197bc3e
Haley <haleyh103@hotmail.com>
2024-02-08 12:05:42 -0500

    NEW UPDATE

--- recyclequest/app/(tabs)/two.tsx
+++ recyclequest/app/(tabs)/two.tsx
@@ -1,13 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Pressable } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Link } from 'expo-router';
 
-export default function TabTwoScreen() {
+export default function TabTwoScreen(props: { onPress: any; title?: "Sign In" | undefined; }) {
+  const { onPress, title = 'Sign In' } = props;
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>THIS WILL BE WHERE \n PPL CAN SIGN IN</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Link href="/three" asChild>
+      <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+    </Link>
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
@@ -27,5 +34,27 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    elevation: 3,
+    backgroundColor: '#1B1F3B',
+    shadowColor: 'darkorange',
+    shadowOffset: {height: -10, width: 100},
+    marginBottom: 25,
+    borderRadius: 12,
+    border: "none",
+    color: "black",
+
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
