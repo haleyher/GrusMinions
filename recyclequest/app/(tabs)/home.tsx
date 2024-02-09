b08e92f13f714b6cfd8f540b259ef8f33f0de57a
timphamle <heytimle@gmail.com>
2024-02-09 09:31:23 -0500

    i changed the background color

--- recyclequest/app/(tabs)/home.tsx
+++ recyclequest/app/(tabs)/home.tsx
@@ -4,9 +4,8 @@ import { Text, View } from '@/components/Themed';
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-     
+      <Text style={styles.title}>Home</Text>     
     </View>
   );
 }
@@ -16,6 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#EDFFCC',
   },
   title: {
     fontSize: 20,
--- recyclequest/app/(tabs)/progress.tsx
+++ recyclequest/app/(tabs)/progress.tsx
@@ -6,7 +6,6 @@ export default function TabTwoScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Progress</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      
     </View>
   );
 }
@@ -16,6 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#EDFFCC',
   },
   title: {
     fontSize: 20,
